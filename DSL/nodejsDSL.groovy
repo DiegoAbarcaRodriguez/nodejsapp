a1110031a72c129804a614064a8ce1cb018d2750
Diego Abarca <112660054+DiegoAbarcaRodriguez@users.noreply.github.com>
2024-08-03 22:25:49 -0600

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/DiegoAbarcaRodriguez/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('DiegoAbarcaRodriguez')
+            node / gitConfigEmail('abarcarodriguezdiego@gmail.com')
         }
     }
     triggers {
